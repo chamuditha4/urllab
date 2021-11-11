4fe5cf8e3214572a95873da370635f36c1f10d7b
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-11-11 21:29:36 +0530

    Copy Text to Clipboard

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function myFunction() {
 
     
   }
+
+  function copy(){
+        /* Get the text field */
+        var copyText = document.getElementById("res");
+
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+
+        /* Alert the copied text */
+        alert("Copied the text: " + copyText.value);
+  }
